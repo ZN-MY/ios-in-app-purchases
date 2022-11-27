506a78784623da3b01c66b52cecccd3b2aaf239a
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-27 15:00:19 +0100

    Initial Commit

---
+++ MY-IAP/MY_IAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_IAPApp.swift
+//  MY-IAP
+//
+//  Created by Zaid Neurothrone on 2022-11-27.
+//
+
+import SwiftUI
+
+@main
+struct MY_IAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
